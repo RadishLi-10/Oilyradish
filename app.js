fae662b02a699c2a2fabda6e48133ee380712859
RadishLi-10 <56287985+RadishLi-10@users.noreply.github.com>
2020-07-15 11:53:02 -0700

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/ipsim/',
+    prefix: '/IpSim/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
